3cb7cd8ea0ca7b206d1a0a88e59b46d77fd52075
cesar <cesar@usemobile.xyz>
2019-04-01 16:50:24 -0300

    starting the burger component

---
+++ src/containers/BurgerBuilder.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+import Burger from '../components/Burger/Burger'
+
+class BurgerBuilder extends Component {
+
+
+  render() {
+    return (
+      <React.Fragment>
+        <Burger />
+        <div>Build Controls</div>
+      </React.Fragment>
+    )
+  }
+}
+
+export default BurgerBuilder
