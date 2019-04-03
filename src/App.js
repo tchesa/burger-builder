4672a416937a71064f3e16dd2c68dc57906cfcd5
tchesa <cesarolimpio@gmail.com>
2019-04-03 11:41:26 -0300

    fetch section 9 starting setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import Layout from './hoc/Layout/Layout'
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+
+import Blog from './containers/Blog/Blog';
 
 class App extends Component {
   render() {
     return (
-      <Layout>
-        <BurgerBuilder />
-      </Layout>
+      <div className="App">
+        <Blog />
+      </div>
     );
   }
 }
