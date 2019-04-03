d0b1abe9aa68948cc68004053d9fdb892bbc0929
tchesa <cesarolimpio@gmail.com>
2019-04-03 09:48:19 -0300

    changing the folder structure

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,7 +4,6 @@ import classes from './Modal.css'
 import Backdrop from '../Backdrop/Backdrop'
 
 class Modal extends Component {
-
   shouldComponentUpdate(nextProps, nextState) {
     return nextProps.show !== this.props.show
   }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 
-import Burger from '../components/Burger/Burger'
-import BuildControls from '../components/Burger/BuildControls/BuildControls'
-import Modal from '../components/UI/Modal/Modal'
-import OrderSummary from '../components/Burger/OrderSummary/OrderSummary'
+import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+import Modal from '../../components/UI/Modal/Modal'
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
 
 const INGREDIENT_PRICES = {
   salad: .5,
