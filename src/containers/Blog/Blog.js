e537091fc7bb5d0c1a0442d4271cbbcac2f09281
tchesa <cesarolimpio@gmail.com>
2019-04-03 16:53:49 -0300

    rendering fetched data to the screen using axios

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,23 +7,35 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+
+  state = {
+    posts: []
+  }
+
+  componentDidMount(){
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+      console.log(response)
+      this.setState({posts: response.data})
+    })
+  }
+
+  render () {
+    const posts = this.state.posts.map(post => <Post key={post.id} title={post.title}/>)
+
+    return (
+      <div>
+        <section className="Posts">
+          {posts}
+        </section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
