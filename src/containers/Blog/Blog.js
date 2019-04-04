2b36ce11c92fc8af026de0fe4128c422f757c5f6
tchesa <cesarolimpio@gmail.com>
2019-04-04 10:52:34 -0300

    setting up the router package

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 // import axios from 'axios'
 import axios from '../../axios'
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -38,17 +35,25 @@ class Blog extends Component {
 
   render () {
     let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>
-    if (!this.state.error) posts = this.state.posts.map(post => <Post key={post.id} title={post.title} author={post.author} clicked={() => this.postSelectedHandler(post.id)}/>)
+    // if (!this.state.error) posts = this.state.posts.map(post => <Post key={post.id} title={post.title} author={post.author} clicked={() => this.postSelectedHandler(post.id)}/>)
 
     return (
-      <div>
-        <section className="Posts">{posts}</section>
-        <section>
+      <div className='Blog'>
+        <header>
+          <nav>
+            <ul>
+              <li><a href='/'>Home</a></li>
+              <li><a href='/new-post'>New Post</a></li>
+            </ul>
+          </nav>
+        </header>
+        {/* <section className="Posts">{posts}</section> */}
+        {/* <section>
           <FullPost id={this.state.selectedPostId}/>
         </section>
         <section>
           <NewPost />
-        </section>
+        </section> */}
       </div>
     );
   }
