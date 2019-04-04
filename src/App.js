72dc1e7aa13f6fd1db6ee01932d4647f143097b6
tchesa <cesarolimpio@gmail.com>
2019-04-04 17:09:50 -0300

    redirect "all-courses" to "courses"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, NavLink, Redirect } from 'react-router-dom'
 
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
@@ -24,6 +24,7 @@ class App extends Component {
             <Switch>
               <Route path='/courses' component={Courses}/>
               <Route path='/users' component={Users}/>
+              <Redirect from='/all-courses' to='/courses'/>
               <Route component={Error}/>
             </Switch>
           </React.Fragment>
@@ -36,7 +37,7 @@ class App extends Component {
           <li>Pass the course title to the "Course" page - pass it as a param or score bonus points by passing it as query params (you need to manually parse them though!)</li> DONE
           <li>Load the "Course" component as a nested component of "Courses"</li> DONE
           <li>Add a 404 error page and render it for any unknown routes</li> DONE
-          <li>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li>
+          <li>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li> DONE
         </ol> */}
       </div>
     );
