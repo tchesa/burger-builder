a19048d3aaf43a2bd5c68e92e8838244090765a5
tchesa <cesarolimpio@gmail.com>
2019-04-04 14:25:52 -0300

    navigating programmatically

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -22,7 +22,7 @@ class Blog extends Component {
         </header>
         <Switch>
           <Route path='/' exact component={Posts}/>
-          <Route path='/new-post' exact component={NewPost}/>
+          <Route path='/new-post' component={NewPost}/>
           <Route path='/:id' exact component={FullPost}/>
         </Switch>
         {/* <section>
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import axios from '../../../axios'
-import { Link } from 'react-router-dom'
 
 import './Posts.css'
 import Post from '../../../components/Post/Post'
@@ -29,12 +28,15 @@ class Posts extends Component {
   }
 
   postSelectedHandler = id => {
-    this.setState({selectedPostId: id})
+    // this.setState({selectedPostId: id})
+    this.props.history.push({
+       pathname: '/' + id
+    })
   }
 
   render () {
     let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>
-    if (!this.state.error) posts = this.state.posts.map(post => <Link to={'/' + post.id} key={post.id}><Post title={post.title} author={post.author} clicked={() => this.postSelectedHandler(post.id)}/></Link>)
+    if (!this.state.error) posts = this.state.posts.map(post => <Post key={post.id} title={post.title} author={post.author} clicked={() => this.postSelectedHandler(post.id)}/>)
 
     return <section className="Posts">{posts}</section>
   }
