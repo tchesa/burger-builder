b5efde79523835f02effbf55a496e7cfcebd9780
tchesa <cesarolimpio@gmail.com>
2019-04-05 17:25:17 -0300

    imporoving visual feedback

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -5,7 +5,7 @@ import classes from './Input.css'
 const input = props => {
   let inputElement = null
   const inputClasses = [classes.InputElement]
-  if (props.invalid && props.shouldValidate) inputClasses.push(classes.Invalid)
+  if (props.touched && props.invalid && props.shouldValidate) inputClasses.push(classes.Invalid)
 
   switch (props.elementType) {
     case 'input': inputElement = <input className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} onChange={props.changed}/>; break
