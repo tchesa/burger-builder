699106ac8cbe07a953bae4f4b55f394942baaf1d
tchesa <cesarolimpio@gmail.com>
2019-04-08 15:54:18 -0300

    working on an error

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -121,6 +121,8 @@ class ContactData extends Component {
 
   checkValidity = (value, rules) => {
     let isValid = false
+    if (!rules) return true
+
     if (rules.required) {
       isValid = value.trim() !== ''
     }
