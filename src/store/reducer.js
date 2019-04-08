1291aaa6ac94e1a1faca9eccf4599457d49938cb
tchesa <cesarolimpio@gmail.com>
2019-04-08 17:49:50 -0300

    switch-case in the reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,24 +3,13 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-  if (action.type === 'INCREMENT') {
-    return {
-      counter: state.counter + 1
-    }
-  } else if (action.type === 'DECREMENT') {
-    return {
-      counter: state.counter - 1
-    }
-  } else if (action.type === 'ADD') {
-    return {
-      counter: state.counter + action.amount
-    }
-  } else if (action.type === 'SUBTRACT') {
-    return {
-      counter: state.counter - action.amount
-    }
+  switch (action.type) {
+    case 'INCREMENT': return { counter: state.counter + 1 }
+    case 'DECREMENT': return { counter: state.counter = 1 }
+    case 'ADD':       return { counter: state.counter + action.amount }
+    case 'SUBTRACT':  return { counter: state.counter - action.amount }
+    default: return state
   }
-  return state
 }
 
 export default reducer
