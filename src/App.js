feb34c1883be564518ffdcd2ffb6d8245b6c7992
tchesa <cesarolimpio@gmail.com>
2019-04-08 16:26:24 -0300

    clone redux--1-start

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import React, { Component } from 'react';
 
-import Layout from './hoc/Layout/Layout'
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
-import Checkout from './containers/Checkout/Checkout'
-import Orders from './containers/Orders/Orders'
+import Counter from './containers/Counter/Counter';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Layout>
-        <Switch>
-          <Route path='/checkout' component={Checkout}/>
-          <Route path='/orders' component={Orders}/>
-          <Route path='/' exact component={BurgerBuilder}/>
-        </Switch>
-      </Layout>
+      <div className="App">
+       <Counter />
+      </div>
     );
   }
 }
