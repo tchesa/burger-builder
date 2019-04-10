f2a86eb6aace580bb1602db9108259e774fa03c4
tchesa <cesarolimpio@gmail.com>
2019-04-10 17:32:13 -0300

    clone project from redux-assignment-2-problem

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux'
-import { Provider } from 'react-redux'
-
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import counterReducer from './store/reducers/counter'
-import resultReducer from './store/reducers/result'
 
-const rootReducer = combineReducers({
-  counter: counterReducer,
-  result: resultReducer
-})
-
-const store = createStore(rootReducer)
-
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
