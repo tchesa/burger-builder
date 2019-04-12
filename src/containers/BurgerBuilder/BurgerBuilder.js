ee439b38956d1bbf7298dd791b1706f8777f4904
tchesa <cesarolimpio@gmail.com>
2019-04-12 09:01:41 -0300

    finishing the reducer for ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,11 +27,11 @@ class BurgerBuilder extends Component {
   }
 
   componentDidMount() {
-    axios.get('https://react-my-burger-bdd91.firebaseio.com/ingredients.json').then(response => {
-      this.setState({ingredients: response.data})
-    }).catch(error => {
-      this.setState({error: true})
-    })
+    // axios.get('https://react-my-burger-bdd91.firebaseio.com/ingredients.json').then(response => {
+    //   this.setState({ingredients: response.data})
+    // }).catch(error => {
+    //   this.setState({error: true})
+    // })
   }
 
   updatePurchaseState = (updatedIngredients) => {
