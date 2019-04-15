a29d9aba3f73fdb7bd435021169c10e25db527b8
tchesa <cesarolimpio@gmail.com>
2019-04-15 11:49:13 -0300

    using utility functions

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes'
+import { updateObject } from '../utility'
 
 const initialState = {
   counter: 0
@@ -6,10 +7,10 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.INCREMENT: return { ...state, counter: state.counter + 1 }
-    case actionTypes.DECREMENT: return { ...state, counter: state.counter - 1 }
-    case actionTypes.ADD:       return { ...state, counter: state.counter + action.amount }
-    case actionTypes.SUBTRACT:  return { ...state, counter: state.counter - action.amount }
+    case actionTypes.INCREMENT: return updateObject(state, { counter: state.counter + 1 }) // utility function example
+    case actionTypes.DECREMENT: return updateObject(state, { counter: state.counter - 1 })
+    case actionTypes.ADD:       return updateObject(state, { counter: state.counter + action.amount })
+    case actionTypes.SUBTRACT:  return updateObject(state, { counter: state.counter - action.amount })
     default: return state
   }
 }
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes'
+import { updateObject } from '../utility'
 
 const initialState = {
   results: []
@@ -6,8 +7,8 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.STORE_RESULT:  return { ...state, results: state.results.concat({ id: new Date(), value: action.result }) }
-    case actionTypes.DELETE_RESULT: return { ...state, results: state.results.filter(result => result.id !== action.id) }
+    case actionTypes.STORE_RESULT:  return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result }) })
+    case actionTypes.DELETE_RESULT: return updateObject(state, { results: state.results.filter(result => result.id !== action.id) })
     default: return state
   }
 }
