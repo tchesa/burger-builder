c16dd4ef22d119b5c7ae541aebdd221b2f9b5ede
tchesa <cesarolimpio@gmail.com>
2019-04-15 09:09:06 -0300

    connectiong the burger builder container to our store

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import axios from '../../axios-orders'
 import Burger from '../../components/Burger/Burger'
@@ -7,6 +8,7 @@ import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
 import Spinner from '../../components/UI/Spinner/Spinner'
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
+import * as actionTypes from '../../store/actions'
 
 const INGREDIENT_PRICES = {
   salad: .5,
@@ -18,7 +20,6 @@ const INGREDIENT_PRICES = {
 class BurgerBuilder extends Component {
 
   state = {
-    ingredients: null,
     totalPrice: 4,
     purchasable: false,
     purchasing: false,
@@ -97,7 +98,7 @@ class BurgerBuilder extends Component {
       this.setState({loading: false, purchasing: false})
     }) */
     const queryParams = []
-    for (let i in this.state.ingredients) queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
+    for (let i in this.props.ingredients) queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.props.ingredients[i]))
     queryParams.push('price=' + this.state.totalPrice)
     const queryString = queryParams.join('&')
     this.props.history.push({
@@ -107,21 +108,34 @@ class BurgerBuilder extends Component {
   }
 
   render() {
-    const disabledInfo = { ...this.state.ingredients }
-    for (let key in disabledInfo) disabledInfo[key] = this.state.ingredients[key] <= 0
+    const disabledInfo = { ...this.props.ingredients }
+    for (let key in disabledInfo) disabledInfo[key] = this.props.ingredients[key] <= 0
 
     const loadingObject = this.state.error? <p>Ingredients can't be loaded</p>: <Spinner />
 
     return (
       <React.Fragment>
-        {this.state.ingredients? <Burger ingredients={this.state.ingredients}/>: loadingObject}
-        <BuildControls ingredientAdded={this.addIngredientHandler} ingredientRemoved={this.removeIngredientHandler} disabledInfo={disabledInfo} price={this.state.totalPrice} purchasable={this.state.purchasable} ordered={this.purchaseHandler}/>
+        {this.props.ingredients? <Burger ingredients={this.props.ingredients}/>: loadingObject}
+        <BuildControls ingredientAdded={this.props.onIngredientAdded} ingredientRemoved={this.props.onIngredientRemoved} disabledInfo={disabledInfo} price={this.state.totalPrice} purchasable={this.state.purchasable} ordered={this.purchaseHandler}/>
         <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-          {this.state.loading? <Spinner />: (this.state.ingredients? <OrderSummary ingredients={this.state.ingredients} purchaseCanceled={this.purchaseCancelHandler} purchaseContinued={this.purchaseContinueHandler} price={this.state.totalPrice}/>: null)}
+          {this.state.loading? <Spinner />: (this.props.ingredients? <OrderSummary ingredients={this.props.ingredients} purchaseCanceled={this.purchaseCancelHandler} purchaseContinued={this.purchaseContinueHandler} price={this.state.totalPrice}/>: null)}
         </Modal>
       </React.Fragment>
     )
   }
 }
 
-export default withErrorHandler(BurgerBuilder, axios)
+const mapStateToProps = state => {
+  return {
+    ingredients: state.ingredients
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onIngredientAdded: ingredientName => dispatch({type: actionTypes.ADD_INGREDIENT, ingredient: ingredientName}),
+    onIngredientRemoved: ingredientName => dispatch({type: actionTypes.REMOVE_INGREDIENT, ingredient: ingredientName})
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(BurgerBuilder, axios))
