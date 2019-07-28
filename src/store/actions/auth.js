694448ec780da1de1002dea1ba9bbab68019906d
tchesa <cesarolimpio@gmail.com>
2019-07-28 20:16:46 -0300

    logging users out

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,20 @@ export const authFail = error => {
   }
 }
 
+export const authLogout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT
+  }
+}
+
+export const checkAuthTimeout = expirationTime => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(authLogout())
+    }, expirationTime * 1000)
+  }
+}
+
 export const auth = (email, password, isSignup) => {
   return dispatch => {
     dispatch(authStart())
@@ -37,6 +51,7 @@ export const auth = (email, password, isSignup) => {
     axios.post(url, authData).then(response => {
       console.log(response)
       dispatch(authSuccess(response.data.idToken, response.data.localId))
+      dispatch(checkAuthTimeout(response.data.expiresIn))
     }).catch(error => {
       console.log(error)
       dispatch(authFail(error.response.data.error))
